14cb74a974deda681df2d95bc460297547aa61b3
bryal <96.bryal@gmail.com>
2015-04-25 14:45:53 +0200

    upd

--- implementors/core/marker/trait.Send.js
+++ implementors/core/marker/trait.Send.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['serial_win'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='serial_win/struct.CommEventWaiter.html' title='serial_win::CommEventWaiter'>CommEventWaiter</a>&lt;'a&gt;","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='serial_win/struct.Connection.html' title='serial_win::Connection'>Connection</a>",];
+implementors['serial_win'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='serial_win/struct.Connection.html' title='serial_win::Connection'>Connection</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
